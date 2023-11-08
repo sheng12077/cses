88951771a0660bb5ff76f86041a217d44c75ff1b
sheng12077 <141042325+sheng12077@users.noreply.github.com>
2023-11-08 22:23:35 +0800

    Update Minimizing Coins.cpp

--- Dynamic Programming/Minimizing Coins.cpp
+++ Dynamic Programming/Minimizing Coins.cpp
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-#define endl "\n"
-#define inf 1e9
-#define maxn 10000005
-
-int coins[1001];
-int dp[1000005];
+#define endl '\n'
+#define inf 2e18
+#define maxn 100005
+#define mod 1000000007
 
 signed main(){
 
@@ -15,16 +13,16 @@ signed main(){
 
     int n,x;
     cin>>n>>x;
-    for(int i=1;i<=n;i++){
-        cin>>coins[i];
+    int coin[105];
+    vector<int>dp(x+1,inf);
+    dp[0]=0;
+    for(int i=0;i<n;i++){
+        cin>>coin[i];
     }
     for(int i=1;i<=x;i++){
-        dp[i]=inf;
-    }
-    for(int i=1;i<=x;i++){
-        for(int j=1;j<=n;j++){
-            if(i-coins[j]>=0){
-                dp[i]=min(dp[i],dp[i-coins[j]]+1);
+        for(int j=0;j<n;j++){
+            if(i-coin[j]>=0){
+                dp[i]=min(dp[i],dp[i-coin[j]]+1);
             }
         }
     }
@@ -34,4 +32,4 @@ signed main(){
     else{
         cout<<dp[x];
     }
-}   
+}
